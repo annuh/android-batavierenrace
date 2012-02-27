0fd4e3e824a5d9b15eabc060c1a3ba1f73ccc622
jochem.elsinga <jochem.elsinga@gmail.com>
2012-02-27 22:38:03 +0000

    Parser voor het parsen van de etappes

---
+++ BataApp/src/com/ut/bataapp/parser/EtappeHandler.java
@@ -0,0 +1,57 @@
+package com.ut.bataapp.parser;
+
+import java.util.ArrayList;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+import com.ut.bataapp.objects.Etappe;
+
+public class EtappeHandler extends DefaultHandler{
+
+	private boolean etappe;
+	private boolean afstand;
+	private boolean type;
+	private boolean ploeg;
+	private boolean tijd;
+	
+	private ArrayList<Etappe> etappes;
+	
+	public ArrayList<Etappe> getParsedData(){
+		return etappes;
+	}
+	
+	@Override
+	public void startElement(String nameSpaceURI, String localName, String qName, Attributes atts) throws SAXException{
+		if(localName.equals("etappe")) this.etappe = true;
+		else if(localName.equals("afstand")) this.afstand = true;
+		else if(localName.equals("type")) this.type = true;
+		else if(localName.equals("ploeg")) this.ploeg = true;
+		else if(localName.equals("tijd")) this.tijd = true;
+	}
+	
+	@Override
+	public void endElement(String nameSpaceURI, String localName, String qName) throws SAXException{
+		if(localName.equals("Etappe")) this.etappe = false;
+		else if(localName.equals("afstand")) this.afstand = false;
+		else if(localName.equals("type")) this.type = false;
+		else if(localName.equals("ploeg")) this.ploeg = false;
+		else if(localName.equals("tijd")) this.tijd = false;
+	}
+	
+	@Override
+	public void characters(char ch[], int start, int length){
+		if(etappe){
+			etappes.add(new Etappe(etappes.size()+1,0,'D',"0","0",null,null,null,null));
+			etappe = false;
+		}
+		if(tijd) etappes.get(etappes.size()-1).setRecordTijd(new String(ch,start,length));
+		else if(ploeg) etappes.get(etappes.size()-1).setRecordTeam(new String(ch,start,length));
+		else if(type) etappes.get(etappes.size()-1).setGeslacht(ch[0]);
+		else if(afstand) etappes.get(etappes.size()-1).setAfstand(Integer.parseInt(new String(ch,start,length)));
+	}
+	
+	@Override
+	public void startDocument() throws SAXException{
+		this.etappes = new ArrayList();
+	}
+}
---
+++ BataApp/src/com/ut/bataapp/parser/Parsing.java
@@ -0,0 +1,61 @@
+package com.ut.bataapp.parser;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.ut.bataapp.objects.Etappe;
+
+public class Parsing extends Activity{
+
+	ArrayList<Etappe> etappes = new ArrayList<Etappe>();
+	String error;
+	
+	public ArrayList<Etappe> parseEtappe(){
+		try{
+			URL url = new URL("http://api.batavierenrace.nl/xml/2011/etappes.xml");
+			SAXParserFactory spf = SAXParserFactory.newInstance();
+			SAXParser sp = spf.newSAXParser();
+			XMLReader xr = sp.getXMLReader();
+			
+			EtappeHandler etappeHandler = new EtappeHandler();
+			xr.setContentHandler(etappeHandler);
+			xr.parse(new InputSource(url.openStream()));
+			
+			etappes = etappeHandler.getParsedData();
+			
+		}catch(SAXException e){
+			e.printStackTrace();
+		}catch(MalformedURLException e){
+			e.printStackTrace();
+		}catch(ParserConfigurationException e) {
+			e.printStackTrace();
+		}catch(IOException e){
+			e.printStackTrace();
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return etappes;
+	}
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		TextView tv = new TextView(this);
+		tv.setText(etappes.toString());
+		setContentView(tv);
+	}
+}
