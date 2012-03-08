dc19bcc35e0908fc281cb633c74b63c2ae30f411
bas6000@gmail.com <bas6000@gmail.com@7f828df8-2beb-5a21-a7cc-0e6728a4b846>
2012-03-08 23:22:10 +0000

    Berichten 3.1

--- trunk/BataApp/src/com/ut/bataapp/MessageArrayAdapter.java
+++ trunk/BataApp/src/com/ut/bataapp/MessageArrayAdapter.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 
 import com.ut.bataapp.objects.Bericht;
 import android.content.Context;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
