795eff9d3cf80559ecb1607f2a1a54adce4481ba
bas6000 <bas6000@gmail.com>
2012-03-08 23:19:29 +0000

    Berichten 3.0

--- BataApp/src/com/ut/bataapp/RouteArrayAdapter.java
+++ BataApp/src/com/ut/bataapp/RouteArrayAdapter.java
@@ -11,6 +11,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 public class RouteArrayAdapter extends ArrayAdapter<String> {
+	
 	private final Context context;
 	private final ArrayList<Etappe> values;
 
