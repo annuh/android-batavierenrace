90795ecf767467f8811ffefe48d11a597753a1b8
AnnevandeVenis <annevandevenis@gmail.com>
2012-03-23 16:06:24 +0000

    + Etappe maps informatie

--- BataApp/src/com/ut/bataapp/fragments/EtappeRoutes.java
+++ BataApp/src/com/ut/bataapp/fragments/EtappeRoutes.java
@@ -4,12 +4,13 @@ import com.actionbarsherlock.app.SherlockFragment;
 import com.ut.bataapp.R;
 import com.ut.bataapp.activities.EtappeActivity;
 import com.ut.bataapp.objects.Etappe;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 public class EtappeRoutes extends SherlockFragment {
     
@@ -21,7 +22,29 @@ public class EtappeRoutes extends SherlockFragment {
     	etappe = ((EtappeActivity) getActivity()).getEtappe();
     	View view = inflater.inflate(R.layout.etappe_routes, container, false);
     	
-    	String [] lopers_maps =  getResources().getStringArray(R.array.looproutes_maps);
+    	String [] lopers_maps =  getResources().getStringArray(R.array.looproutes_earth);
+    	
+    	ImageView lopers_map = (ImageView) view.findViewById(R.id.lopers_maps);
+    	lopers_map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            	Intent mapLopersroute = new Intent(Intent.ACTION_VIEW); 
+                Uri uri0 = Uri.parse("http://maps.google.com/maps?f=d&source=s_d&saddr=51.82002+5.8679&daddr=51.82939+5.94184&geocode=FfS1FgMdfIlZAA%3BFY7aFgMdUKpaAA&aq=&sll=51.82939,5.94184&sspn=0.01037,0.027874&vpsrc=0&t=h&hl=nl&mra=ls&ie=UTF8&z=14&dirflg=d"); 
+                mapLopersroute.setData(uri0); 
+                startActivity(Intent.createChooser(mapLopersroute, "Lopersroute maps"));
+            }
+        });
+    	
+    	ImageView lopers_earth = (ImageView) view.findViewById(R.id.lopers_earth);
+    	lopers_earth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            	Intent mapLopersroute = new Intent(Intent.ACTION_VIEW); 
+                Uri uri0 = Uri.parse("http://g.co/maps/bkwev"); 
+                mapLopersroute.setData(uri0); 
+                startActivity(Intent.createChooser(mapLopersroute, "Lopersroute routebeschrijving"));
+            }
+        });
     	
     	return view;
     	
