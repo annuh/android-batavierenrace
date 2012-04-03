5622a2d100a6f938f54962556ad02bdfeb8c6527
jochem.elsinga <jochem.elsinga@gmail.com>
2012-04-03 14:31:44 +0000

    Zware updates:  -Object looptijd aangepast zodat het simpeler wordt  -PloegHandler code beter gemaakt en aan Looptijd aangepast  -Handler formaat aanpassingen  -EtappeUitslagHandler aan gewerkt nog niet af  -Api aangepast aan Looptijd aanpassingen  -LooptijdAdapter aangepast aan Looptijd aanpassingen

--- BataApp/src/com/ut/bataapp/parser/EtappeUitslagHandler.java
+++ BataApp/src/com/ut/bataapp/parser/EtappeUitslagHandler.java
@@ -1,19 +1,69 @@
 package com.ut.bataapp.parser;
 
 import java.util.ArrayList;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+
+import com.ut.bataapp.objects.Etappe;
 import com.ut.bataapp.objects.Looptijd;
 import com.ut.bataapp.objects.Response;
+import com.ut.bataapp.objects.Team;
 
 public class EtappeUitslagHandler extends Handler{
 
+	private Looptijd looptijd;
 	private ArrayList<Looptijd> uitslagen;
 	
-	public EtappeUitslagHandler(String path) {
-		super(path);
-	}
+	private boolean uitslag;
+	private boolean foutcode;
+	private boolean tijd;
+	private boolean snelheid;
+	
+	private boolean ploeg;
+	private boolean startnummer;
+	private boolean naam;
+	private boolean startgroep;
 
+	private int etappeID;
+	
+	public EtappeUitslagHandler(String path,int id) {
+		super(path);
+		etappeID = id;
+	}
+	
 	public Response getParsedData() {
 		return new Response(uitslagen,this.status);
 	}
 
+
+	@Override
+	public void startElement(String nameSpaceURI, String localName, String qName, Attributes atts) throws SAXException{
+		if(localName.equals("uitslag")) this.uitslag = true;
+		if(localName.equals("foutcode")) this.foutcode = true;
+		if(localName.equals("startnummer")) this.startnummer = true;
+		if(localName.equals("naam")) this.naam = true;
+		if(localName.equals("startgroep")) this.startgroep = true;
+	}
+	
+	@Override
+	public void endElement(String nameSpaceURI, String localName, String qName) throws SAXException{
+		if(localName.equals("foutcode")) this.foutcode = false;
+		if(localName.equals("startnummer")) this.startnummer = false;
+		if(localName.equals("naam")) this.naam = false;
+		if(localName.equals("startgroep")) this.startgroep = false;
+	}
+	
+	@Override
+	public void characters(char ch[], int start, int length){
+		if(uitslag){
+			looptijd = new Looptijd();
+			uitslag = false;
+		}
+	}
+	
+	@Override
+	public void startDocument() throws SAXException{
+		
+	}
 }
--- BataApp/src/com/ut/bataapp/parser/Handler.java
+++ BataApp/src/com/ut/bataapp/parser/Handler.java
@@ -24,173 +24,189 @@ import com.ut.bataapp.objects.Response;
 import android.os.Environment;
 import android.util.Log;
 
-public abstract class Handler extends DefaultHandler{
-String location;
-public int status = Response.NOK_NO_DATA;
-private boolean parsed=false;//only for parse method!!
-public Handler(String path){
-        this.location = path;
-}
-public Handler(){}
+public abstract class Handler extends DefaultHandler {
+	String location;
+	public int status = Response.NOK_NO_DATA;
+	private boolean parsed = false;// only for parse method!!
 
-public boolean parse(){
-        boolean result = false;
-        try{
-                InputSource input = getInputSource(location);
-                if(!parsed||!(status==Response.OK_NO_UPDATE)){
-                SAXParserFactory spf = SAXParserFactory.newInstance();
-                SAXParser sp = spf.newSAXParser();
-                XMLReader xr = sp.getXMLReader();
-                
-                xr.setContentHandler(this);
-                xr.parse(input);
-                parsed = true;
-                }
-                result = true;
-        }catch(SAXException e){
-                Log.d("SimpleEtappe","SAXEception: "+e.getMessage());
-        }catch(MalformedURLException e){
-                Log.d("SimpleEtappe","MalformedUrlException: "+e.getMessage());
-        }catch(ParserConfigurationException e){
-                Log.d("SimpleEtappe","ParserConfigException: "+e.getMessage());
-        }catch(IOException e){
-                Log.d("SimpleEtappe","IOExcpetion: "+e.getMessage());
-        }
-        Log.d("parser","parse geeft als resultaat:"+result+ " bij klasse: "+this.getClass());
-        return result;
-}
-/**
-* Deze functie levert een inputsource voor de parser
-* @param path het pad naar de file zoals bijvoorbeeld: "/etappes.xml"
- * @throws IOException 
-*
-*/
-private InputSource getInputSource(String path) throws IOException{
-File sdFile = getFile(path);
-InputSource result = null;
-if(sdFile == null){
-        status = Response.NOK_OLD_DATA;
-        result = new InputSource(new URL(api.getURL()+path).openStream());
-}else{
-if(sdFile.exists()){
-        if(isNewest(path)){
-                status = Response.OK_NO_UPDATE;
-                result = new InputSource(new FileInputStream(sdFile));
-        }
-        else{
-                if(downloadToSD(path)){
-                        status = Response.OK_UPDATE;
-                        //Het is gelukt om de file te downloaden, gebruik dit bestand.
-                        result = new InputSource(new FileInputStream(sdFile));  
-                }else{
-                //Het is niet gelukt de file te downloaden, gebruik oude versie
-                        status = Response.NOK_OLD_DATA;
-                result = new InputSource(new FileInputStream(sdFile));
-                }
-        }
-}
-else{
-        if(downloadToSD(path)){
-                //Het is gelukt om de file te downloaden, gebruik dit bestand.
-                result = new InputSource(new FileInputStream(sdFile));  
-                status = Response.OK_UPDATE;
-        }else{
-                status = Response.NOK_NO_DATA;
-            //Het is niet gelukt de file te downloaden, wat nu?
-        }       
-}
-}
-return result;
+	public Handler(String path) {
+		this.location = path;
+	}
 
-}
-private File getFile(String path){
-        File result = null;
-        if (Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())) {
-                //De locatie van de file is de root van de sdkaart+dexmlmap+het gegeven path.
-            File map = new File(Environment.getExternalStorageDirectory().getPath()+api.getSDmap());
-            //Maak directory aan als die nog niet bestaat
-            if(!map.exists()){
-                Log.d("parser","dts:xmlMap bestaat nog niet");
-                map.mkdir();
-            }
-            
-                result = new File(map.getPath()+'/'+path);
-                if(!result.getParentFile().exists()){
-                        result.getParentFile().mkdir();
-                }
-                //sdFile is de file waar de nieuw file heengeschreven word.
-            //"externe" media is gemount dus we kunnen lezen en schrijven naar file.
-                //Activity act = new Activity();
-                //result =  new File(act.getExternalFilesDir(api.getSDmap()).getPath()+path);
-                //result = new File(Environment.getExternalStorageDirectory().getPath()+api.getSDmap()+path);
-        }
-        Log.d("parser","test"+result.getPath());
-                return result;
-}
+	public Handler() {
+	}
 
-/**
-* Deze functie controleerd of de huidige versie van een bestand de nieuwste is.
-*/
-private boolean isNewest(String path){
-        /**boolean result = false;
-        if(#seconds from lastupdate of updatefile >api.getThreshold()){
-                downloadToSD("update.xml");
-        }
-        if(#timestamp voor path in update.xml <= timestamp in path op SD){
-                return true;
-        }
-        return result;*/
-        return true;
-}
+	public boolean parse() {
+		boolean result = false;
+		try {
+			InputSource input = getInputSource(location);
+			if (!parsed || !(status == Response.OK_NO_UPDATE)) {
+				SAXParserFactory spf = SAXParserFactory.newInstance();
+				SAXParser sp = spf.newSAXParser();
+				XMLReader xr = sp.getXMLReader();
 
+				xr.setContentHandler(this);
+				xr.parse(input);
+				parsed = true;
+			}
+			result = true;
+		} catch (SAXException e) {
+			Log.d("SimpleEtappe", "SAXEception: " + e.getMessage());
+		} catch (MalformedURLException e) {
+			Log.d("SimpleEtappe", "MalformedUrlException: " + e.getMessage());
+		} catch (ParserConfigurationException e) {
+			Log.d("SimpleEtappe", "ParserConfigException: " + e.getMessage());
+		} catch (IOException e) {
+			Log.d("SimpleEtappe", "IOExcpetion: " + e.getMessage());
+		}
+		Log.d("parser", "parse geeft als resultaat:" + result + " bij klasse: "
+				+ this.getClass());
+		return result;
+	}
 
-/**
-* Deze methode probeert de file gegeven door path te downloaden naar de sdcard.
-* @result true als het downloaden gelukt is, false als het dat niet is
-*/
-private boolean downloadToSD(String path){
-boolean result = false;
-try{
-        Log.d("parser",api.getURL()+path);
-        URL url = new URL(api.getURL()+path);//de file die gedownload moet worden
-        //nieuwe verbinding:
-        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-        //setup connectie:
-    urlConnection.setRequestMethod("GET");
-    urlConnection.setDoOutput(true);
+	/**
+	 * Deze functie levert een inputsource voor de parser
+	 * 
+	 * @param path
+	 *            het pad naar de file zoals bijvoorbeeld: "/etappes.xml"
+	 * @throws IOException
+	 * 
+	 */
+	private InputSource getInputSource(String path) throws IOException {
+		File sdFile = getFile(path);
+		InputSource result = null;
+		if (sdFile == null) {
+			status = Response.NOK_OLD_DATA;
+			result = new InputSource(new URL(api.getURL() + path).openStream());
+		} else {
+			if (sdFile.exists()) {
+				if (isNewest(path)) {
+					status = Response.OK_NO_UPDATE;
+					result = new InputSource(new FileInputStream(sdFile));
+				} else {
+					if (downloadToSD(path)) {
+						status = Response.OK_UPDATE;
+						// Het is gelukt om de file te downloaden, gebruik dit
+						// bestand.
+						result = new InputSource(new FileInputStream(sdFile));
+					} else {
+						// Het is niet gelukt de file te downloaden, gebruik
+						// oude versie
+						status = Response.NOK_OLD_DATA;
+						result = new InputSource(new FileInputStream(sdFile));
+					}
+				}
+			} else {
+				if (downloadToSD(path)) {
+					// Het is gelukt om de file te downloaden, gebruik dit
+					// bestand.
+					result = new InputSource(new FileInputStream(sdFile));
+					status = Response.OK_UPDATE;
+				} else {
+					status = Response.NOK_NO_DATA;
+					// Het is niet gelukt de file te downloaden, wat nu?
+				}
+			}
+		}
+		return result;
 
-    // connect!
-    urlConnection.connect();
-        
-    
-        File sdFile = getFile(path);
-        //sdFile is de file waar de nieuw file heengeschreven word.
-    
-    FileOutputStream fileOutput = new FileOutputStream(sdFile);
+	}
 
-    //InputStream vanaf internet
-    InputStream inputStream = urlConnection.getInputStream();
+	private File getFile(String path) {
+		File result = null;
+		if (Environment.MEDIA_MOUNTED.equals(Environment
+				.getExternalStorageState())) {
+			// De locatie van de file is de root van de sdkaart+dexmlmap+het
+			// gegeven path.
+			File map = new File(Environment.getExternalStorageDirectory()
+					.getPath() + api.getSDmap());
+			// Maak directory aan als die nog niet bestaat
+			if (!map.exists()) {
+				Log.d("parser", "dts:xmlMap bestaat nog niet");
+				map.mkdir();
+			}
 
-    //aanmaken leesbuffer.
-    byte[] buffer = new byte[1024];
-    int bufferLength = 0; //tijdelijke lengte van de buffer
+			result = new File(map.getPath() + '/' + path);
+			if (!result.getParentFile().exists()) {
+				result.getParentFile().mkdir();
+			}
+			// sdFile is de file waar de nieuw file heengeschreven word.
+			// "externe" media is gemount dus we kunnen lezen en schrijven naar
+			// file.
+			// Activity act = new Activity();
+			// result = new
+			// File(act.getExternalFilesDir(api.getSDmap()).getPath()+path);
+			// result = new
+			// File(Environment.getExternalStorageDirectory().getPath()+api.getSDmap()+path);
+		}
+		Log.d("parser", "test" + result.getPath());
+		return result;
+	}
 
-    //lees door de input buffer en schrijf naar de File.
-    while ( (bufferLength = inputStream.read(buffer)) > 0 ) {
-         fileOutput.write(buffer, 0, bufferLength);
-    }
-    //Sluit de outputSream
-    fileOutput.close();
+	/**
+	 * Deze functie controleerd of de huidige versie van een bestand de nieuwste
+	 * is.
+	 */
+	private boolean isNewest(String path) {
+		/**
+		 * boolean result = false; if(#seconds from lastupdate of updatefile
+		 * >api.getThreshold()){ downloadToSD("update.xml"); } if(#timestamp
+		 * voor path in update.xml <= timestamp in path op SD){ return true; }
+		 * return result;
+		 */
+		return true;
+	}
 
-        result = true;  
-}catch(MalformedURLException mue){
-        Log.d("parser","malformedurl: "+mue.toString());
-}catch(IOException ioe){
-        Log.d("parser","ioexeption: "+ioe.toString());
-}
-Log.d("parser","result: "+ result);
+	/**
+	 * Deze methode probeert de file gegeven door path te downloaden naar de
+	 * sdcard.
+	 * 
+	 * @result true als het downloaden gelukt is, false als het dat niet is
+	 */
+	private boolean downloadToSD(String path) {
+		boolean result = false;
+		try {
+			Log.d("parser", api.getURL() + path);
+			URL url = new URL(api.getURL() + path);// de file die gedownload
+													// moet worden
+			// nieuwe verbinding:
+			HttpURLConnection urlConnection = (HttpURLConnection) url
+					.openConnection();
+			// setup connectie:
+			urlConnection.setRequestMethod("GET");
+			urlConnection.setDoOutput(true);
 
-return result;  
-}
+			// connect!
+			urlConnection.connect();
+
+			File sdFile = getFile(path);
+			// sdFile is de file waar de nieuw file heengeschreven word.
+
+			FileOutputStream fileOutput = new FileOutputStream(sdFile);
+
+			// InputStream vanaf internet
+			InputStream inputStream = urlConnection.getInputStream();
+
+			// aanmaken leesbuffer.
+			byte[] buffer = new byte[1024];
+			int bufferLength = 0; // tijdelijke lengte van de buffer
+
+			// lees door de input buffer en schrijf naar de File.
+			while ((bufferLength = inputStream.read(buffer)) > 0) {
+				fileOutput.write(buffer, 0, bufferLength);
+			}
+			// Sluit de outputSream
+			fileOutput.close();
+
+			result = true;
+		} catch (MalformedURLException mue) {
+			Log.d("parser", "malformedurl: " + mue.toString());
+		} catch (IOException ioe) {
+			Log.d("parser", "ioexeption: " + ioe.toString());
+		}
+		Log.d("parser", "result: " + result);
+
+		return result;
+	}
 
 }
