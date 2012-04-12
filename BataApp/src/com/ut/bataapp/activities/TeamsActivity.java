3fcf3088f15376dfc0df284b165138ca886c6028
AnnevandeVenis <annevandevenis@gmail.com>
2012-04-12 12:23:41 +0000

    + Sorteer functie in Etappe Looptijd TODO: Klopt nog niet helemaal, meerdere teams op dezelfde stand?

--- BataApp/src/com/ut/bataapp/activities/TeamsActivity.java
+++ BataApp/src/com/ut/bataapp/activities/TeamsActivity.java
@@ -39,111 +39,111 @@ import com.ut.bataapp.objects.Response;
 import com.ut.bataapp.objects.Team;
 
 public class TeamsActivity extends SherlockListActivity  {
-	
-	 private final int MENU_SEARCH = Menu.FIRST;
-	 private final int MENU_SORT_NAAM = Menu.FIRST + 1;
-	 private final int MENU_SORT_START = Menu.FIRST + 2;
-	 private Response teams = null;
-	 private EditText filterText = null;
-	 private TeamAdapter adapter = null;
-	 
-	 
-	 
-	 
-   @Override
-   public void onCreate(Bundle savedInstanceState) {
-	   setTitle("Teams");
-	   super.onCreate(savedInstanceState);
-	   getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-	   this.getListView().setFastScrollEnabled(true);
-	   this.setContentView(R.layout.listview_team);
-	   new getTeams().execute();  	   
-   }
-   
-   @Override
-   public void onListItemClick(ListView l, View v, int position, long id) {
-	   Intent intent = new Intent(getApplicationContext(), TeamActivity.class);
-       intent.putExtra("index", v.getId());
-       startActivity(intent);
-   }
-   
-   @Override
-   public boolean onCreateOptionsMenu(Menu menu) {	
- 	   menu.add(0,MENU_SEARCH,Menu.NONE, R.string.ab_zoeken)
- 	   	.setIcon(R.drawable.ic_action_search)
- 	   	.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS | MenuItem.SHOW_AS_ACTION_COLLAPSE_ACTION_VIEW);
- 	   
- 	  SubMenu subMenu1 = menu.addSubMenu(R.string.ab_sorteren);
-      subMenu1.add(0,MENU_SORT_NAAM,Menu.NONE, "Naam");
-      subMenu1.add(0,MENU_SORT_START,Menu.NONE, "Startnummer");
 
-     subMenu1.getItem()
-      .setIcon(R.drawable.ic_action_sort)
-      .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
- 	   return true;
-    }
-   
+	private final int MENU_SEARCH = Menu.FIRST;
+	private final int MENU_SORT_NAAM = Menu.FIRST + 1;
+	private final int MENU_SORT_START = Menu.FIRST + 2;
+	private Response teams = null;
+	private EditText filterText = null;
+	private TeamAdapter adapter = null;
+
+
+
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		setTitle("Teams");
+		super.onCreate(savedInstanceState);
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+		this.getListView().setFastScrollEnabled(true);
+		this.setContentView(R.layout.listview_team);
+		new getTeams().execute();  	   
+	}
+
+	@Override
+	public void onListItemClick(ListView l, View v, int position, long id) {
+		Intent intent = new Intent(getApplicationContext(), TeamActivity.class);
+		intent.putExtra("index", v.getId());
+		startActivity(intent);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {	
+		menu.add(0,MENU_SEARCH,Menu.NONE, R.string.ab_zoeken)
+		.setIcon(R.drawable.ic_action_search)
+		.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS | MenuItem.SHOW_AS_ACTION_COLLAPSE_ACTION_VIEW);
+
+		SubMenu subMenu1 = menu.addSubMenu(R.string.ab_sorteren);
+		subMenu1.add(0,MENU_SORT_NAAM,Menu.NONE, "Naam");
+		subMenu1.add(0,MENU_SORT_START,Menu.NONE, "Startnummer");
+
+		subMenu1.getItem()
+		.setIcon(R.drawable.ic_action_sort)
+		.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+		return true;
+	}
+
 	@SuppressWarnings("unchecked")
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		Log.d("Keyboard", String.valueOf(item.getItemId()));
 		switch (item.getItemId()) {
-			case android.R.id.home:
-				Utils.goHome(getApplicationContext());
-	            break;
-			case MENU_SEARCH:
-				item.setActionView(R.layout.search_box);
-				filterText = (EditText) item.getActionView().findViewById(R.id.search_box);
-				filterText.addTextChangedListener(filterTextWatcher);
-				setKeyboardFocus(filterText);
-				break;
-			case MENU_SORT_NAAM:
-				Collections.sort( (ArrayList<Team>) teams.getResponse(),new Comparator<Team>() {
-				    public int compare(Team arg0, Team arg1) {
-				    	return arg0.getNaam().compareTo(arg1.getNaam());
-				    }
-				});
-				adapter = new TeamAdapter(TeamsActivity.this, (ArrayList<Team>) teams.getResponse());
-			    setListAdapter(adapter);
-			    adapter.notifyDataSetChanged();
-				break;
-			case MENU_SORT_START:
-				Collections.sort((ArrayList<Team>) teams.getResponse(),new Comparator<Team>() {
-				    public int compare(Team arg0, Team arg1) {
-				    	return (arg0.getStartnummer()<arg1.getStartnummer() ? -1 : (arg0.getStartnummer()==arg1.getStartnummer() ? 0 : 1));
-				    }
-				});
-				adapter = new TeamAdapter(TeamsActivity.this, (ArrayList<Team>) teams.getResponse());
-			    setListAdapter(adapter);
-			    adapter.notifyDataSetChanged();
-			    
+		case android.R.id.home:
+			Utils.goHome(getApplicationContext());
+			break;
+		case MENU_SEARCH:
+			item.setActionView(R.layout.search_box);
+			filterText = (EditText) item.getActionView().findViewById(R.id.search_box);
+			filterText.addTextChangedListener(filterTextWatcher);
+			setKeyboardFocus(filterText);
+			break;
+		case MENU_SORT_NAAM:
+			Collections.sort( (ArrayList<Team>) teams.getResponse(),new Comparator<Team>() {
+				public int compare(Team arg0, Team arg1) {
+					return arg0.getNaam().compareTo(arg1.getNaam());
+				}
+			});
+			adapter = new TeamAdapter(TeamsActivity.this, (ArrayList<Team>) teams.getResponse());
+			setListAdapter(adapter);
+			adapter.notifyDataSetChanged();
+			break;
+		case MENU_SORT_START:
+			Collections.sort((ArrayList<Team>) teams.getResponse(),new Comparator<Team>() {
+				public int compare(Team arg0, Team arg1) {
+					return (arg0.getStartnummer()<arg1.getStartnummer() ? -1 : (arg0.getStartnummer()==arg1.getStartnummer() ? 0 : 1));
+				}
+			});
+			adapter = new TeamAdapter(TeamsActivity.this, (ArrayList<Team>) teams.getResponse());
+			setListAdapter(adapter);
+			adapter.notifyDataSetChanged();
+
 		}
 		return super.onOptionsItemSelected(item);
 	}
-	
+
 	public static void setKeyboardFocus(final EditText primaryTextField) {
-		   (new Handler()).postDelayed(new Runnable() {
-		     public void run() {
-		       primaryTextField.dispatchTouchEvent(MotionEvent.obtain(SystemClock.uptimeMillis(), SystemClock.uptimeMillis(), MotionEvent.ACTION_DOWN, 0, 0, 0));
-		       primaryTextField.dispatchTouchEvent(MotionEvent.obtain(SystemClock.uptimeMillis(), SystemClock.uptimeMillis(), MotionEvent.ACTION_UP , 0, 0, 0));
-		     }
-		   }, 100);
-		 }
-	
+		(new Handler()).postDelayed(new Runnable() {
+			public void run() {
+				primaryTextField.dispatchTouchEvent(MotionEvent.obtain(SystemClock.uptimeMillis(), SystemClock.uptimeMillis(), MotionEvent.ACTION_DOWN, 0, 0, 0));
+				primaryTextField.dispatchTouchEvent(MotionEvent.obtain(SystemClock.uptimeMillis(), SystemClock.uptimeMillis(), MotionEvent.ACTION_UP , 0, 0, 0));
+			}
+		}, 100);
+	}
+
 	private class getTeams extends AsyncTask<Void, Void, Void> {  
 		private ProgressDialog progressDialog;  
 		protected void onPreExecute() {  
 			progressDialog = ProgressDialog.show(TeamsActivity.this,  
-			  "Bezig met laden", "Teams worden opgehaald...", true);  
+					"Bezig met laden", "Teams worden opgehaald...", true);  
 		}
-		
+
 		@SuppressWarnings("unchecked")
 		@Override  
 		protected Void doInBackground(Void... arg0) {
 			teams = (Response) api.getTeams();
 			return null;       
 		}
-		
+
 		@SuppressWarnings("unchecked")
 		@Override  
 		protected void onPostExecute(Void result) {
@@ -153,26 +153,26 @@ public class TeamsActivity extends SherlockListActivity  {
 				Utils.old_data(getApplicationContext());
 			}
 			adapter = new TeamAdapter(TeamsActivity.this, (ArrayList<Team>) teams.getResponse());
-			
+
 			setListAdapter(adapter);
 			progressDialog.dismiss();
-		
+
 		}
 	}
-	
+
 	private TextWatcher filterTextWatcher = new TextWatcher() {
 
-	    public void afterTextChanged(Editable s) {
-	    }
+		public void afterTextChanged(Editable s) {
+		}
 
-	    public void beforeTextChanged(CharSequence s, int start, int count,
-	            int after) {
-	    }
+		public void beforeTextChanged(CharSequence s, int start, int count,
+				int after) {
+		}
 
-	    public void onTextChanged(CharSequence s, int start, int before,
-	            int count) {
-	        adapter.getFilter().filter(s);
-	    }
+		public void onTextChanged(CharSequence s, int start, int before,
+				int count) {
+			adapter.getFilter().filter(s);
+		}
 
 	};
 }
--- BataApp/src/com/ut/bataapp/fragments/EtappeLooptijdenFragment.java
+++ BataApp/src/com/ut/bataapp/fragments/EtappeLooptijdenFragment.java
@@ -1,23 +1,20 @@
 package com.ut.bataapp.fragments;
 
 import java.util.ArrayList;
-
+import java.util.Collections;
+import java.util.Comparator;
 import com.actionbarsherlock.app.SherlockListFragment;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuInflater;
+import com.actionbarsherlock.view.MenuItem;
+import com.actionbarsherlock.view.SubMenu;
 import com.ut.bataapp.R;
 import com.ut.bataapp.Utils;
 import com.ut.bataapp.activities.EtappeActivity;
-import com.ut.bataapp.activities.TeamActivity;
-import com.ut.bataapp.activities.TeamsActivity;
 import com.ut.bataapp.adapters.EtappeLooptijdAdapter;
-import com.ut.bataapp.adapters.KlassementAdapter;
-import com.ut.bataapp.adapters.LooptijdAdapter;
-import com.ut.bataapp.adapters.TeamAdapter;
 import com.ut.bataapp.api.api;
 import com.ut.bataapp.objects.Looptijd;
 import com.ut.bataapp.objects.Response;
-import com.ut.bataapp.objects.Team;
-
-import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
@@ -30,24 +27,63 @@ import android.widget.ListView;
 
 public class EtappeLooptijdenFragment extends SherlockListFragment {
 
-	ArrayList<Looptijd> looptijden;
+	private ArrayList<Looptijd> looptijden;
+	private EtappeLooptijdAdapter adapter = null;
+	private final int MENU_SORT_NAAM = Menu.FIRST + 1;
+	private final int MENU_SORT_STAND = Menu.FIRST + 2;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		inflater.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE); 
-		//setListAdapter(new EtappeLooptijdAdapter(this.getActivity().getApplicationContext(), looptijden));
-
 		View view = inflater.inflate(R.layout.listview_etappe_looptijden, null); 
-		
-		
 		return view;
-
 	}
 
 	@Override
 	public void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setHasOptionsMenu(true);
 		new getEtappeLooptijden().execute();
+	}
+
+
+	@Override
+	public void onCreateOptionsMenu (Menu menu, MenuInflater inflater) {
+		SubMenu subMenu1 = menu.addSubMenu(R.string.ab_sorteren);
+		subMenu1.add(0,MENU_SORT_NAAM,Menu.NONE, "Naam");
+		subMenu1.add(0,MENU_SORT_STAND,Menu.NONE, "Uitslag");
+
+		subMenu1.getItem()
+		.setIcon(R.drawable.ic_action_sort)
+		.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		Log.d("Keyboard", String.valueOf(item.getItemId()));
+		switch (item.getItemId()) {
+		case MENU_SORT_NAAM:
+			Collections.sort(looptijden,new Comparator<Looptijd>() {
+				public int compare(Looptijd arg0, Looptijd arg1) {
+					return arg0.getTeamNaam().compareTo(arg1.getTeamNaam());
+				}
+			});
+			adapter = new EtappeLooptijdAdapter(getActivity().getApplicationContext(), looptijden);
+			setListAdapter(adapter);
+			adapter.notifyDataSetChanged();
+			break;
+		case MENU_SORT_STAND:
+			Collections.sort(looptijden,new Comparator<Looptijd>() {
+				public int compare(Looptijd arg0, Looptijd arg1) {
+					return (arg0.getEtappeStand()<arg1.getEtappeStand() ? -1 : (arg0.getEtappeStand()==arg1.getEtappeStand() ? 0 : 1));
+				}
+			});
+			adapter = new EtappeLooptijdAdapter(getActivity().getApplicationContext(), looptijden);
+			setListAdapter(adapter);
+			adapter.notifyDataSetChanged();
+
+		}
+		return super.onOptionsItemSelected(item);
 	}
 
 	@Override
@@ -56,33 +92,24 @@ public class EtappeLooptijdenFragment extends SherlockListFragment {
 		intent.putExtra("index", v.getId());
 		startActivity(intent);
 	}
-	
+
 	private class getEtappeLooptijden extends AsyncTask<Void, Void, Void> {
 		Response response;
-		private ProgressDialog progressDialog;  
-		protected void onPreExecute() {  
-			//progressDialog = ProgressDialog.show(TeamsActivity.this,  
-			//  "Bezig met laden", "Teams worden opgehaald...", true);  
-		}
-		
-		@SuppressWarnings("unchecked")
+
 		@Override  
 		protected Void doInBackground(Void... arg0) {
 			response = (Response) api.getUitslagenVanEtappe(((EtappeActivity) getActivity()).getEtappe().getId());
 			return null;
 		}
-		
+
 		@SuppressWarnings("unchecked")
 		@Override  
 		protected void onPostExecute(Void result) {
 			if(Utils.checkResponse(getActivity().getApplicationContext(), response)) {
 				looptijden = (ArrayList<Looptijd>) response.getResponse();
-
-				setListAdapter(new EtappeLooptijdAdapter(getActivity().getApplicationContext(), looptijden));
-				//progressDialog.dismiss();
+				adapter = new EtappeLooptijdAdapter(getActivity().getApplicationContext(), looptijden);
+				setListAdapter(adapter);
 			}
-			
-		
 		}
 	}
 
