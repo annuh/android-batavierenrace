a5cf951e22cd99d66a2dc9f63c3704ad9bd1aaa8
erikpleijsier@gmail.com <erikpleijsier@gmail.com@7f828df8-2beb-5a21-a7cc-0e6728a4b846>
2012-04-13 13:35:47 +0000

    -berichten werkt nu (technisch gezien)

--- trunk/BataApp/src/com/ut/bataapp/activities/BerichtenActivity.java
+++ trunk/BataApp/src/com/ut/bataapp/activities/BerichtenActivity.java
@@ -1,28 +1,35 @@
 package com.ut.bataapp.activities;
 
 import java.util.ArrayList;
+
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import com.actionbarsherlock.app.SherlockListActivity;
 import com.actionbarsherlock.view.MenuItem;
 import com.ut.bataapp.MainActivity;
+import com.ut.bataapp.Utils;
 import com.ut.bataapp.MainActivity.OverridePendingTransition;
 import com.ut.bataapp.adapters.BerichtAdapter;
+import com.ut.bataapp.adapters.KlassementAdapter;
 import com.ut.bataapp.api.api;
 import com.ut.bataapp.objects.Bericht;
+import com.ut.bataapp.objects.Klassement;
+import com.ut.bataapp.objects.Response;
 
 
 public class BerichtenActivity extends SherlockListActivity  {
-	
+	private ArrayList<Bericht> values;
+	private BerichtAdapter ba = null;
    /** Called when the activity is first created. */
    @Override
    public void onCreate(Bundle savedInstanceState) {
 	   getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 	   setTitle("Berichten");
 	   super.onCreate(savedInstanceState);
-	   ArrayList<Bericht> values = (ArrayList<Bericht>) api.getBerichten().getResponse();
-	   setListAdapter(new BerichtAdapter(BerichtenActivity.this, values));
+	   new getBerichten().execute();
    }
    
    	@Override
@@ -41,5 +48,30 @@ public class BerichtenActivity extends SherlockListActivity  {
 		
 		return super.onOptionsItemSelected(item);
 	}
+   	private class getBerichten extends AsyncTask<Void, Void, Void> {
+		Response response;
+		private ProgressDialog progressDialog;  
+		protected void onPreExecute() {  
+			progressDialog = ProgressDialog.show(BerichtenActivity.this,  
+					"Bezig met laden", "Berichten worden opgehaald...", true);  
+		}
 
+		@Override  
+		protected Void doInBackground(Void... arg0) {
+			response = api.getBerichten();
+			return null;
+		}
+
+		@Override  
+		protected void onPostExecute(Void result) {
+			if(Utils.checkResponse(getApplicationContext(), response)) {
+				values =  (ArrayList<Bericht>) response.getResponse();
+				ba = new BerichtAdapter(BerichtenActivity.this, values);
+				setListAdapter(ba);
+				progressDialog.dismiss();
+			}
+			
+
+		}
+	}
 }
