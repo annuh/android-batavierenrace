c3cae1678d1876f64cf7ae68e1713ae7b1a3f641
AnnevandeVenis <annevandevenis@gmail.com>
2012-04-16 12:35:15 +0000

    Klassement + Headers fixed + Zoeken + Sorteren + Annuleren laden

--- BataApp/src/com/ut/bataapp/activities/KlassementActivity.java
+++ BataApp/src/com/ut/bataapp/activities/KlassementActivity.java
@@ -1,11 +1,23 @@
 package com.ut.bataapp.activities;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnCancelListener;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ListView;
 import com.actionbarsherlock.app.SherlockListActivity;
 import com.actionbarsherlock.view.Menu;
@@ -16,12 +28,16 @@ import com.ut.bataapp.Utils;
 import com.ut.bataapp.adapters.KlassementAdapter;
 import com.ut.bataapp.api.api;
 import com.ut.bataapp.objects.Klassement;
+import com.ut.bataapp.objects.KlassementItem;
+import com.ut.bataapp.objects.Looptijd;
 import com.ut.bataapp.objects.Response;
 
 public class KlassementActivity extends SherlockListActivity  {
 
+	private final int MENU_SEARCH = Menu.FIRST;
 	private final int MENU_SORT_NAAM = Menu.FIRST + 1;
-	private final int MENU_SORT_PLAATS = Menu.FIRST + 2;
+	private final int MENU_SORT_STAND = Menu.FIRST + 2;
+	private EditText filterText = null;
 	private String naam;
 	private Klassement klassement;
 	private KlassementAdapter adapter = null;
@@ -46,10 +62,13 @@ public class KlassementActivity extends SherlockListActivity  {
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {	
+		menu.add(0,MENU_SEARCH,Menu.NONE, R.string.ab_zoeken)
+		.setIcon(R.drawable.ic_action_search)
+		.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS | MenuItem.SHOW_AS_ACTION_COLLAPSE_ACTION_VIEW);
 
 		SubMenu subMenu1 = menu.addSubMenu(R.string.ab_sorteren);
 		subMenu1.add(0,MENU_SORT_NAAM,Menu.NONE, "Naam");
-		subMenu1.add(0,MENU_SORT_PLAATS,Menu.NONE, "Plaats");
+		subMenu1.add(0,MENU_SORT_STAND,Menu.NONE, "Plaats");
 
 		subMenu1.getItem()
 		.setIcon(R.drawable.ic_action_sort)
@@ -64,43 +83,90 @@ public class KlassementActivity extends SherlockListActivity  {
 		case android.R.id.home:
 			Utils.goHome(getApplicationContext());
 			break;
-		/*	
-		case MENU_SORT_NAAM:
-			Collections.sort( (ArrayList<Team>) teams.getResponse(),new Comparator<Team>() {
-				public int compare(Team arg0, Team arg1) {
-					return arg0.getNaam().compareTo(arg1.getNaam());
-				}
-			});
-			adapter = new TeamAdapter(TeamsActivity.this, (ArrayList<Team>) teams.getResponse());
-			setListAdapter(adapter);
-			adapter.notifyDataSetChanged();
+		case MENU_SEARCH:
+			item.setActionView(R.layout.search_box);
+			filterText = (EditText) item.getActionView().findViewById(R.id.search_box);
+			filterText.addTextChangedListener(filterTextWatcher);
+			setKeyboardFocus(filterText);
 			break;
-		case MENU_SORT_START:
-			Collections.sort((ArrayList<Team>) teams.getResponse(),new Comparator<Team>() {
-				public int compare(Team arg0, Team arg1) {
-					return (arg0.getStartnummer()<arg1.getStartnummer() ? -1 : (arg0.getStartnummer()==arg1.getStartnummer() ? 0 : 1));
-				}
-			});
-			adapter = new TeamAdapter(TeamsActivity.this, (ArrayList<Team>) teams.getResponse());
-			setListAdapter(adapter);
-			adapter.notifyDataSetChanged();
-		 */
+		case MENU_SORT_NAAM:
+			sortList(klassement.getUitslag(), "naam");
+			break;
+		case MENU_SORT_STAND:
+			sortList(klassement.getUitslag(), "stand");
+			break;
 		}
 		return super.onOptionsItemSelected(item);
 	}
+
+	public static void setKeyboardFocus(final EditText primaryTextField) {
+		(new Handler()).postDelayed(new Runnable() {
+			public void run() {
+				primaryTextField.dispatchTouchEvent(MotionEvent.obtain(SystemClock.uptimeMillis(), SystemClock.uptimeMillis(), MotionEvent.ACTION_DOWN, 0, 0, 0));
+				primaryTextField.dispatchTouchEvent(MotionEvent.obtain(SystemClock.uptimeMillis(), SystemClock.uptimeMillis(), MotionEvent.ACTION_UP , 0, 0, 0));
+			}
+		}, 100);
+	}
+
+	public void sortList(ArrayList<KlassementItem> list, final String sort) {
+		Collections.sort(list,new Comparator<KlassementItem>() {
+			public int compare(KlassementItem arg0, KlassementItem arg1) {
+				if(sort.equals("naam"))
+					return arg0.getTeamNaam().compareTo(arg1.getTeamNaam());
+				else if(sort.equals("stand"))
+					return (arg0.getPlaats()<arg1.getPlaats() ? -1 : (arg0.getPlaats()==arg1.getPlaats() ? 0 : 1));
+				else
+					return 0;
+			}
+		});
+		adapter = new KlassementAdapter(KlassementActivity.this, list);
+		setListAdapter(adapter);
+		adapter.notifyDataSetChanged();
+	}
+
+	OnCancelListener cancelListener=new OnCancelListener(){
+		@Override
+		public void onCancel(DialogInterface arg0){
+			finish();
+		}
+	};
+
+	private TextWatcher filterTextWatcher = new TextWatcher() {
+
+		public void afterTextChanged(Editable s) {
+		}
+
+		public void beforeTextChanged(CharSequence s, int start, int count,
+				int after) {
+		}
+
+		public void onTextChanged(CharSequence s, int start, int before,
+				int count) {
+			adapter.getFilter().filter(s);
+		}
+
+	};
 
 
 	private class getKlassement extends AsyncTask<Void, Void, Void> {
 		Response<Klassement> response;
 		private ProgressDialog progressDialog;  
-		protected void onPreExecute() {  
+		protected void onPreExecute() {
 			progressDialog = ProgressDialog.show(KlassementActivity.this,  
-					"Bezig met laden", "Klassement wordt opgehaald...", true);  
+					"Bezig met laden", "Klassement wordt opgehaald...", true);
+			progressDialog.setCancelable(true);
+		    progressDialog.setOnCancelListener(new OnCancelListener() {
+		        public void onCancel(DialogInterface dialog) {
+		        	cancel(true);
+		        	Utils.goHome(KlassementActivity.this);
+		        }
+		    });
 		}
 
 		@Override  
 		protected Void doInBackground(Void... arg0) {
-			response = api.getKlassementByNaam(naam);
+			while(!isCancelled())
+				response = api.getKlassementByNaam(naam);
 			return null;
 		}
 
@@ -112,7 +178,7 @@ public class KlassementActivity extends SherlockListActivity  {
 				setListAdapter(adapter);
 				progressDialog.dismiss();
 			}
-			
+
 
 		}
 	}
--- BataApp/src/com/ut/bataapp/adapters/KlassementAdapter.java
+++ BataApp/src/com/ut/bataapp/adapters/KlassementAdapter.java
@@ -3,23 +3,29 @@ package com.ut.bataapp.adapters;
 import java.util.ArrayList;
 import com.ut.bataapp.R;
 import com.ut.bataapp.objects.KlassementItem;
-
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Filter;
 import android.widget.TextView;
 
 public class KlassementAdapter extends ArrayAdapter<KlassementItem> {
 
 	private final Context context;
-	private final ArrayList<KlassementItem> values;
+	private ArrayList<KlassementItem> values;
+	private ArrayList<KlassementItem> filteredItems;
+	private KlassementFilter filter;
 
 	public KlassementAdapter(Context context, ArrayList<KlassementItem> values) {
 		super(context, R.layout.row_looptijd, values);
 		this.context = context;
-		this.values = values;
+		this.values = new ArrayList<KlassementItem>();
+		this.values.addAll(values);
+		filteredItems = new ArrayList<KlassementItem>();
+		filteredItems.addAll(values);
+		getFilter();
 	}
 
 	@Override
@@ -39,6 +45,60 @@ public class KlassementAdapter extends ArrayAdapter<KlassementItem> {
 		tijd.setText(values.get(position).getTijd());
 
 		return rowView;
+	}
+	
+	@Override
+	public Filter getFilter() {
+		if (filter == null){
+			filter = new KlassementFilter();
+		}
+		return filter;
+	}
+
+
+	private class KlassementFilter extends Filter{
+
+		@Override
+		protected FilterResults performFiltering(CharSequence constraint) {
+
+			constraint = constraint.toString().toLowerCase();
+			FilterResults result = new FilterResults();
+			if(constraint != null && constraint.toString().length() > 0)
+			{
+				ArrayList<KlassementItem> filteredItems = new ArrayList<KlassementItem>();
+
+				for(int i = 0, l = values.size(); i < l; i++)
+				{
+					KlassementItem m = values.get(i);
+					if(m.getTeamNaam().toLowerCase().contains(constraint) || String.valueOf(m.getTeamStartNummer()).contains(constraint))
+						filteredItems.add(m);
+				}
+				result.count = filteredItems.size();
+				result.values = filteredItems;
+			}
+			else
+			{
+				synchronized(this)
+				{
+					result.values = values;
+					result.count = values.size();
+				}
+			}
+			return result;
+		}
+
+		@SuppressWarnings("unchecked")
+		@Override
+		protected void publishResults(CharSequence constraint, FilterResults results) {
+			filteredItems = (ArrayList<KlassementItem>)results.values;
+			notifyDataSetChanged();
+			clear();
+			for (int i = 0; i <filteredItems.size(); i++){
+				add(filteredItems.get(i));
+			}
+			notifyDataSetInvalidated();
+
+		}
 	}
 
 }
