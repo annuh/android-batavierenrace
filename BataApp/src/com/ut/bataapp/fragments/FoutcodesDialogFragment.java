ef91329a4e0377d4bd9d16fe9e56ef5c46d3ef6d
AnnevandeVenis <annevandevenis@gmail.com>
2012-04-18 13:13:09 +0000

    + Header fix voor Team/Looptijden :D

---
+++ BataApp/src/com/ut/bataapp/fragments/FoutcodesDialogFragment.java
@@ -0,0 +1,20 @@
+package com.ut.bataapp.fragments;
+
+import com.ut.bataapp.R;
+
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FoutcodesDialogFragment extends DialogFragment {
+
+	/*@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		View v = inflater.inflate(R.layout.foutcodes, container, false);
+		this.getDialog().setTitle("Foutcodes");
+		return v;
+	}*/
+
+}
