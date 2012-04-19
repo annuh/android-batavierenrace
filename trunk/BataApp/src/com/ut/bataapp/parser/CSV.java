18ee37b9645c7b18ebbfff6d4f1a82a00d7a3a7b
jochem.elsinga@gmail.com <jochem.elsinga@gmail.com@7f828df8-2beb-5a21-a7cc-0e6728a4b846>
2012-04-19 14:53:33 +0000

    route beschrijving in tekst (plaatjes van bas komen nog)

--- trunk/BataApp/src/com/ut/bataapp/parser/CSV.java
+++ trunk/BataApp/src/com/ut/bataapp/parser/CSV.java
@@ -3,25 +3,27 @@ package com.ut.bataapp.parser;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Scanner;
+
+import android.util.Log;
+
 import com.ut.bataapp.objects.EtappeRoute;
 
 public class CSV {
 	
 	public EtappeRoute parse(InputStream csv){
-				
+	
 		Scanner scanner = new Scanner(csv);
-		
-		String firstLine = scanner.nextLine();
-		Scanner scannerFirstLine = new Scanner(firstLine);
-		int etappeNummer = scannerFirstLine.nextInt();
-		
+
 		String TekstVoorTabel = "";
 		boolean tabelBegonnen = false;
 		String newLine = "";
 		
+		scanner.nextLine();
+		
 		while(!tabelBegonnen){
 			
 			newLine = scanner.nextLine();
+			Log.i("debugger", newLine);
 			if(newLine.equals("Afst;Omschrijving;Locatie")){
 				tabelBegonnen = true;
 			} else{
@@ -37,10 +39,11 @@ public class CSV {
 		
 		while(!tabelEinde){
 			newLine = scanner.nextLine();
+			Log.i("debugger", newLine);
 			
 			String[] rij = newLine.split(";");
 			
-			if(rij[1].equals("") && rij[2].equals("")){
+			if(rij.length == 1 || (rij[1].equals("") && rij[2].equals(""))){
 				tabelEinde = true;
 				TekstNaTabel += rij[0];
 			} else{
@@ -55,7 +58,8 @@ public class CSV {
 			TekstNaTabel += zin[0];			
 		}
 		
-		return new EtappeRoute(etappeNummer, TekstVoorTabel, tabel, TekstNaTabel);
+		return new EtappeRoute(TekstVoorTabel, tabel, TekstNaTabel);		
+		
 	}
 
 }
