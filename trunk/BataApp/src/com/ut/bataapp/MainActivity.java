bc189e7329b0c6a6c9b64e3118e3329b46fb9575
sihidahtc@gmail.com <sihidahtc@gmail.com@7f828df8-2beb-5a21-a7cc-0e6728a4b846>
2012-04-27 10:42:54 +0000

    * Geen R van ABS

--- trunk/BataApp/src/com/ut/bataapp/MainActivity.java
+++ trunk/BataApp/src/com/ut/bataapp/MainActivity.java
@@ -15,7 +15,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.actionbarsherlock.R;
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuItem;
